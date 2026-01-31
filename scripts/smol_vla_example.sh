2430b2c6639877bd5b954eadc2751088f7c3ecec
Timothy Wroge <tiw51@pitt.edu>
2026-01-31 14:47:17 -0800

    update scripts

--- scripts/smol_vla_example.sh
+++ scripts/smol_vla_example.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 python lerobot/src/lerobot/scripts/lerobot_train.py \
-  --policy.path=lerobot/smolvla_base \
+  --policy.type=smolvla \
   --dataset.repo_id=lerobot/svla_so100_stacking \
-  --batch_size=64 \
-  --steps=20000  # 10% of training budget
+  --batch_size=4 \
+  --steps=20000 \
+  --policy.push_to_hub=false 
